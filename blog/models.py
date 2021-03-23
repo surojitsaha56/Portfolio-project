21334ee2f11bb14afe9fef8686c25079bcc70f37
Surojit Saha <sahasurojit06@gmail.com>
2021-03-24 02:15:01 +0530

    Added blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+#title
+#date
+#body
+#image
+#Add the blog app to the settings
+#Create a migration
+#Migrate
+#Add to the admin
+
+class Blog(models.Model):
+    title= models.CharField(max_length=20)
+    date= models.DateTimeField()
+    body= models.TextField(max_length=200)
+    image=models.ImageField(upload_to="images/")
+
