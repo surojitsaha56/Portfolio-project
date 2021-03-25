95fc1e2bfd16826d76514087399050fc84a79f28
Surojit Saha <sahasurojit06@gmail.com>
2021-03-25 18:55:59 +0530

    Added static files

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,13 @@ from django.db import models
 class Blog(models.Model):
     title= models.CharField(max_length=20)
     date= models.DateTimeField()
-    body= models.TextField(max_length=200)
+    body= models.TextField(max_length=100)
     image=models.ImageField(upload_to="images/")
+
+    def date2(self):
+        return self.date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
 
